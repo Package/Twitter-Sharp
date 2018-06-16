7e5b2eeb1275bfdd089b9658a0f347032be74ed3
Package <ryan.dean1000@outlook.com>
2018-06-16 14:46:44 +0100

    Uploading Project

---
+++ TwitterSharp.Data/Context/TwitterSharpContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TwitterSharp.Data.Models;
+
+namespace TwitterSharp.Data.Context
+{
+
+    public class TwitterSharpContext : IdentityDbContext<ApplicationUser>
+    {
+        public TwitterSharpContext()
+            : base("DbConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static TwitterSharpContext Create()
+        {
+            return new TwitterSharpContext();
+        }
+    }
+}
---
+++ TwitterSharp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TwitterSharp.MvcApplication" Language="C#" %>
